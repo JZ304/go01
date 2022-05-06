5ba5238c81710f01ce2cb8c0e57508e8115cea43
oven <13880614578@163.com>
2022-05-06 17:34:38 +0800

    初始化路由

---
+++ bootstrap/route.go
@@ -0,0 +1,44 @@
+package bootstrap
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/jz304/gohub/routes"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRoute(router *gin.Engine) {
+
+	//注册全局中间件
+	registerGlobalMiddleWare(router)
+
+	// 注册API路由
+	routes.RegisterAPIRoutes(router)
+
+	// 配置404路由
+	setup404Handler(router)
+
+}
+
+func registerGlobalMiddleWare(router *gin.Engine) {
+	router.Use(
+		gin.Logger(),
+		gin.Recovery(),
+	)
+}
+
+func setup404Handler(router *gin.Engine) {
+	router.NoRoute(func(c *gin.Context) {
+		acceptString := c.Request.Header.Get("Accept")
+		if strings.Contains(acceptString, "text/html") {
+			c.String(http.StatusOK, "页面返回")
+		} else {
+			c.JSON(http.StatusNotFound, gin.H{
+				"error_code":    404,
+				"error_message": "路由未定义",
+			})
+		}
+	})
+}
--- main.go
+++ main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"net/http"
-	"strings"
+	"fmt"
+
+	"github.com/jz304/gohub/bootstrap"
 
 	"github.com/gin-gonic/gin"
 )
@@ -18,28 +19,41 @@ func main() {
 	// })
 	// r.Run()
 
-	r := gin.New()
+	// r := gin.New()
 
-	r.Use(gin.Logger(), gin.Recovery())
+	// r.Use(gin.Logger(), gin.Recovery())
 
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"Hello": "world!",
-		})
-	})
+	// r.GET("/", func(c *gin.Context) {
+	// 	c.JSON(http.StatusOK, gin.H{
+	// 		"Hello": "world!",
+	// 	})
+	// })
 
-	// 404处理
-	r.NoRoute(func(c *gin.Context) {
-		acceptString := c.Request.Header.Get("Accept")
-		if strings.Contains(acceptString, "text/html") {
-			c.String(http.StatusNotFound, "页面返回404")
-		} else {
-			c.JSON(http.StatusOK, gin.H{
-				"error_code": 404,
-				"error_info": "路由未定义",
-			})
-		}
-	})
+	// // 404处理
+	// r.NoRoute(func(c *gin.Context) {
+	// 	acceptString := c.Request.Header.Get("Accept")
+	// 	if strings.Contains(acceptString, "text/html") {
+	// 		c.String(http.StatusNotFound, "页面返回404")
+	// 	} else {
+	// 		c.JSON(http.StatusOK, gin.H{
+	// 			"error_code": 404,
+	// 			"error_info": "路由未定义",
+	// 		})
+	// 	}
+	// })
 
-	r.Run(":8000")
+	// r.Run(":8000")
+
+	router := gin.New()
+
+	// 初始化路由绑定
+	bootstrap.SetupRoute(router)
+
+	//运行服务
+	err := router.Run(":3000")
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
 }
