890edcfc714d5f3c3cea8e11b2fd550ba00a9161
oven <13880614578@163.com>
2022-05-06 16:03:46 +0800

    集成 gin

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
@@ -9,12 +12,34 @@ func main() {
 	// r := gin.Default()
 
 	// r.GET("/", func(c *gin.Context) {
-	// 	c.JSON(http.StatusOK, gin.H{
-	// 		"Hello": "World!",
+	// 	c.JSON(http.StatusInternalServerError, gin.H{
+	// 		"Hello": "World!", "name": "zjx",
 	// 	})
 	// })
+	// r.Run()
 
-	// r.run()
+	r := gin.New()
 
-	fmt.Println("Helloo!!!222")
+	r.Use(gin.Logger(), gin.Recovery())
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"Hello": "world!",
+		})
+	})
+
+	// 404处理
+	r.NoRoute(func(c *gin.Context) {
+		acceptString := c.Request.Header.Get("Accept")
+		if strings.Contains(acceptString, "text/html") {
+			c.String(http.StatusNotFound, "页面返回404")
+		} else {
+			c.JSON(http.StatusOK, gin.H{
+				"error_code": 404,
+				"error_info": "路由未定义",
+			})
+		}
+	})
+
+	r.Run(":8000")
 }
